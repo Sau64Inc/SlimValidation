952cd999fa1f0dc62af408c6de91ea0c2d4cc01e
Sau64 <sauron@gxzone.com>
2026-04-03 14:37:51 -0300

    Fix V class to use delegation instead of extending final Validator class

--- src/V.php
+++ src/V.php
@@ -7,19 +7,26 @@ namespace Sau64Inc\SlimValidation;
 use Respect\Validation\Validator;
 
 /**
- * Validation helper that extends Respect\Validation\Validator.
- * Provides IDE autocompletion for custom rules registered via Factory.
+ * IDE helper that proxies static calls to Respect\Validation\Validator.
+ * Since Validator is a final class, this uses delegation instead of inheritance.
  *
  * Usage: use Sau64Inc\SlimValidation\V;
  *
- * @method static V fileUploaded()
- * @method static V fileMaxSize(int|string $maxSize)
- * @method static V fileMimeType(array $allowedTypes)
- * @method static V numeric()
- * @method static V internationalPhone()
- * @method static V uInt32()
- * @method static V int32()
+ * @method static Validator fileUploaded()
+ * @method static Validator fileMaxSize(int|string $maxSize)
+ * @method static Validator fileMimeType(array $allowedTypes)
+ * @method static Validator numeric()
+ * @method static Validator internationalPhone()
+ * @method static Validator uInt32()
+ * @method static Validator int32()
  */
-class V extends Validator
+class V
 {
+    /**
+     * @param mixed[] $arguments
+     */
+    public static function __callStatic(string $ruleName, array $arguments): Validator
+    {
+        return Validator::__callStatic($ruleName, $arguments);
+    }
 }
